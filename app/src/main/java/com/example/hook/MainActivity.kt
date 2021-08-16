f03c94950d012c869cfae4067095b0d2f0fc2da0
何超 <hec@shuyilink.com>
2021-08-16 15:45:31 +0800

    优化aidl的逻辑

--- app/src/main/java/com/example/hook/MainActivity.kt
+++ app/src/main/java/com/example/hook/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import android.view.View
 import com.example.hook.aidl.HookManagerStub
 import com.example.hook.aidl.IHook
@@ -20,19 +21,24 @@ class MainActivity : AppCompatActivity() {
 
     private var hookManager: IHookManager? = null
 
+    private var conn: ServiceConnection? = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.i("TAG", "onServiceConnected: 服务绑定成功...")
+            hookManager = HookManagerStub.asInterface(service)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.i("TAG", "onServiceConnected: 服务绑定失败...")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bindService(Intent(this, HookService::class.java), object : ServiceConnection {
-            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                hookManager = HookManagerStub.asInterface(service)
-            }
-
-            override fun onServiceDisconnected(name: ComponentName?) {
-                println("$name")
-            }
-        }, BIND_AUTO_CREATE)
+        conn?.apply {
+            bindService(Intent(this@MainActivity, HookService::class.java), this, BIND_AUTO_CREATE)
+        }
     }
 
     fun addData(view: View) {
@@ -44,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         } catch (e: Exception) {
-            e.printStackTrace()
+            Log.i("TAG", "addData : addHook出错，e: ${e.message}")
         }
     }
 
@@ -53,7 +59,12 @@ class MainActivity : AppCompatActivity() {
             val data = hookManager?.getHooks()
             println("获取的数据：$data")
         } catch (e: Exception) {
-            e.printStackTrace()
+            Log.i("TAG", "addData : getHooks出错，e: ${e.message}")
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        conn = null
     }
 }
